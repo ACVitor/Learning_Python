3e702d593a5cda8aff3779843ba38050bc5d19bb
Vitor Abreu <carvalho.vitor631@gmail.com>
2022-06-26 14:56:34 -0300

    Learning_Python_V006
    Ajustes finais em 021.

--- General_Exercises/021_Play_music.py
+++ General_Exercises/021_Play_music.py
@@ -1,13 +1,13 @@
 import pygame
-#def blue_danube():
-#    '''This funcition plays one of the most beautiful songs ever created - The Blue Danube Waltz'''
-pygame.init()
-pygame.mixer.init()
-pygame.mixer.music.load('021_the_blue_danube_waltz.mp3')
-pygame.mixer.music.set_volume(2)
-pygame.mixer.music.play()
-input()
-pygame.event.wait()
+def blue_danube():
+    '''This funcition plays one of the most beautiful songs ever created - The Blue Danube Waltz'''
+    pygame.init()
+    pygame.mixer.init()
+    pygame.mixer.music.load('021_the_blue_danube_waltz.mp3')
+    pygame.mixer.music.set_volume(2)
+    pygame.mixer.music.play()
+    input()
+    pygame.event.wait()
 
-#blue_danube()
+blue_danube()
 
